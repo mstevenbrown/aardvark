7337909c43f98603619ed1deae488735301d5dde
msbrown <msbrown@msnkbrown.net>
2025-01-29 14:16:26 -0600

    added custom strings file

---
+++ config/custom_strings_inc.php
@@ -0,0 +1,46 @@
+<?php
+
+#####################################################
+# OPEN GROUP CUSTOMIZATIONS BEGIN HERE
+#####################################################
+
+$s_severity_enum_string = '10:Comment,50:Editorial,70:Objection';
+$s_reproducibility_enum_string = '30:Error,50:Omission,70:Clarification Requested,90:Enhancement Request';
+$s_status_enum_string = '10:New,20:Under Review,40:Interpretation Required,50:Resolution Proposed,80:Resolved,85:Applied,90:Closed';
+$s_resolution_enum_string = '10:Open,20:Accepted,30:Reopened,40:Accepted As Marked,60:Duplicate,80:Future Enhancement,85:Withdrawn,90:Rejected';
+
+$s_email_reproducibility = 'Type';
+$s_reproducibility = 'Type';
+$s_select_reproducibility = 'Select Type';
+
+$s_additional_information_updated = 'Desired Action Updated';
+$s_additional_information = 'Desired Action';
+
+$s_New_bug_title = 'New Issue';
+$s_Interpretation_Required_bug_title = 'Interpretation Required for Issue';
+$s_Resolution_Proposed_bug_title = 'Resolution Proposed for Issue';
+$s_Under_Review_bug_title = 'Issue is Under Review';
+$s_Resolved_bug_title = 'Resolve Issue';
+$s_Applied_bug_title = 'Issue Applied';
+$s_Closed_bug_title = 'Close Issue';
+$s_New_bug_button = 'New Issue';
+$s_Interpretation_Required_bug_button = 'Interpretation Required';
+$s_Resolution_Proposed_bug_button = 'Resolution Proposed';
+$s_Under_Review_bug_button = 'Under Review';
+$s_Resolved_bug_button = 'Resolve Issue';
+$s_Applied_bug_button = 'Issue Applied';
+$s_Closed_bug_button = 'Close Issue';
+
+$s_email_desired_action = 'Desired Action';
+
+$s_print_all_bug_page_link = 'Create A Printable Report';
+
+$s_email_notification_title_for_status_bug_Applied = 'The following issue has a resolution that has been APPLIED.';
+$s_email_notification_title_for_status_bug_Closed = 'The following issue has been CLOSED.';
+$s_email_notification_title_for_status_bug_Resolution_Proposed = 'The following issue has a PROPOSED RESOLUTION.';
+$s_email_notification_title_for_status_bug_Under_Review = 'The following issue is UNDER REVIEW.';
+$s_email_notification_title_for_status_bug_Interpretation_Required = 'The following issue NEEDS AN INTERPRETATION.';
+$s_email_notification_title_for_status_bug_Resolved = 'The following issue has been RESOLVED.';
+
+
+?>
