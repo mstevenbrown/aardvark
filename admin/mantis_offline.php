870105ca9539d8bf4d0fa161b51fd2ed081b06ff
msbrown <msbrown@msnkbrown.net>
2025-01-31 11:15:38 -0600

    adding the maintenence notification file

---
+++ admin/mantis_offline.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *   To make MantisBT 'offline' simply create a file called
+ *   'mantis_offline.php' in the MantisBT root directory.
+ *   Users are redirected to that file if it exists.
+ *   If you have to test MantisBT while it's offline, add the
+ *   parameter 'mbadmin=1' to the URL.
+ * 
+ * So, move/copy this file to the main directory.
+ */
+
+   echo "The Austin Group Issue Tool is currently offline for maintenance.";
+?>
+
